4cdf320767cd5c40461070bb6ae39d4ec0b6a7e9
Chris Fodor <cfodor@sdsu.edu>
2024-03-26 18:10:59 -0700

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // External tunables.
+    static public float m_fMaxSpeed = 0.015f;
+    public float m_fSlowSpeed = m_fMaxSpeed * 0.66f;
+    public float m_fIncSpeed = 0.00002f;
+    public float m_fMagnitudeFast = 0.6f;
+    public float m_fMagnitudeSlow = 0.06f;
+    public float m_fFastRotateSpeed = 0.2f;
+    public float m_fFastRotateMax = 10.0f;
+    public float m_fDiveTime = 0.3f;
+    public float m_fDiveRecoveryTime = 0.5f;
+    public float m_fDiveDistance = 3.0f;
+
+    // Internal variables.
+    public Vector3 m_vDiveStartPos;
+    public Vector3 m_vDiveEndPos;
+    public float m_fAngle;
+    public float m_fSpeed;
+    public float m_fTargetSpeed;
+    public float m_fTargetAngle;
+    public eState m_nState;
+    public float m_fDiveStartTime;
+
+    public enum eState : int
+    {
+        kMoveSlow,
+        kMoveFast,
+        kDiving,
+        kRecovering,
+        kNumStates
+    }
+
+    private Color[] stateColors = new Color[(int)eState.kNumStates]
+    {
+        new Color(0,     0,   0),
+        new Color(255, 255, 255),
+        new Color(0,     0, 255),
+        new Color(0,   255,   0),
+    };
+
+    public bool IsDiving()
+    {
+        return (m_nState == eState.kDiving);
+    }
+
+    void CheckForDive()
+    {
+        if (Input.GetMouseButton(0) && (m_nState != eState.kDiving && m_nState != eState.kRecovering))
+        {
+            // Start the dive operation
+            m_nState = eState.kDiving;
+            m_fSpeed = 0.0f;
+
+            // Store starting parameters.
+            m_vDiveStartPos = transform.position;
+            m_vDiveEndPos = m_vDiveStartPos - (transform.right * m_fDiveDistance);
+            m_fDiveStartTime = Time.time;
+        }
+    }
+
+    void Start()
+    {
+        // Initialize variables.
+        m_fAngle = 0;
+        m_fSpeed = 0;
+        m_nState = eState.kMoveSlow;
+    }
+
+    void UpdateDirectionAndSpeed()
+    {
+        // Get relative positions between the mouse and player
+        Vector3 vScreenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 vScreenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        Vector2 vOffset = new Vector2(transform.position.x - vScreenPos.x, transform.position.y - vScreenPos.y);
+
+        // Find the target angle being requested.
+        m_fTargetAngle = Mathf.Atan2(vOffset.y, vOffset.x) * Mathf.Rad2Deg;
+
+        // Calculate how far away from the player the mouse is.
+        float fMouseMagnitude = vOffset.magnitude / vScreenSize.magnitude;
+
+        // Based on distance, calculate the speed the player is requesting.
+        if (fMouseMagnitude > m_fMagnitudeFast)
+        {
+            m_fTargetSpeed = m_fMaxSpeed;
+        }
+        else if (fMouseMagnitude > m_fMagnitudeSlow)
+        {
+            m_fTargetSpeed = m_fSlowSpeed;
+        }
+        else
+        {
+            m_fTargetSpeed = 0.0f;
+        }
+    }
+
+    void Update()
+    {
+        switch (m_nState)
+        {
+        }
+        GetComponent<Renderer>().material.color = stateColors[(int)m_nState];
+    }
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Player m_player;
+    public enum eState : int
+    {
+        kIdle,
+        kHopStart,
+        kHop,
+        kCaught,
+        kNumStates
+    }
+
+    private Color[] stateColors = new Color[(int)eState.kNumStates]
+   {
+        new Color(255, 0,   0),
+        new Color(0,   255, 0),
+        new Color(0,   0,   255),
+        new Color(255, 255, 255)
+   };
+
+    // External tunables.
+    public float m_fHopTime = 0.2f;
+    public float m_fHopSpeed = 6.0f;
+    public float m_fScaredDistance = 0.0f;
+    public int m_nMaxMoveAttempts = 50;
+
+    // Internal variables.
+    public eState m_nState;
+    public float m_fHopStart;
+    public Vector3 m_vHopStartPos;
+    public Vector3 m_vHopEndPos;
+
+    void Start()
+    {
+        // Setup the initial state and get the player GO.
+        m_nState = eState.kIdle;
+        m_player = GameObject.FindObjectOfType(typeof(Player)) as Player;
+    }
+
+    void Update()
+    {
+        switch (m_nState)
+        {
+        }
+        GetComponent<Renderer>().material.color = stateColors[(int)m_nState];
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        // Check if this is the player (in this situation it should be!)
+        if (collision.gameObject == GameObject.Find("Player"))
+        {
+            // If the player is diving, it's a catch!
+            if (m_player.IsDiving())
+            {
+                m_nState = eState.kCaught;
+                transform.parent = m_player.transform;
+                transform.localPosition = new Vector3(0.0f, -0.5f, 0.0f);
+            }
+        }
+    }
+}
